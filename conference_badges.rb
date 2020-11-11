6b9c5db1965a51ca048a1e6563bd3cb663c03012
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-11 15:40:05 -0500

    commit tree f4d1b570eb8806b11121e8e3390c301e9b626b15

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(name)
+    names = []
+    name.each do |name|
+        names << "Hello, my name is #{name}."
+    end
+    names
+end
+
+wack = ["Jim", "Jan", "Jake", "Jubi"]
+
+
+def assign_rooms(name)
+    name_room = []
+    name.each_with_index do |name, index|
+        name_room << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    end
+    name_room
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |attendees|
+        puts attendees
+    end
+    assign_rooms(attendees).each do |attendees|
+        puts attendees
+    end
+end
+   
+
+printer(wack)
